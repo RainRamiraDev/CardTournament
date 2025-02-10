9e033cd0b856b293a91beb9e3caa05c258dd4878
RainRamiraDev <114005@tecnicatura.frc.utn.edu.ar>
2025-02-10 16:21:27 -0300

    .

--- CTDao/obj/Debug/net7.0/CTDao.AssemblyInfo.cs
+++ CTDao/obj/Debug/net7.0/CTDao.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CTDao")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1f65c3c622d37c8e25338698e662cd7a2880248e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2f6b6bc7049cc10a46cf5810a4397328ed570463")]
 [assembly: System.Reflection.AssemblyProductAttribute("CTDao")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CTDao")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
