e927555b0669fe9ff5e98932c2f8fee602b9104e
RainRamiraDev <114005@tecnicatura.frc.utn.edu.ar>
2025-02-26 16:14:02 -0300

    .

--- CTApp/Controllers/GameController.cs
+++ CTApp/Controllers/GameController.cs
@@ -32,5 +32,5 @@ namespace CTApp.Controllers
             return Ok(response);
         }
     }
-    }
+    
 }
--- CTApp/obj/Debug/net8.0/CTApp.AssemblyInfo.cs
+++ CTApp/obj/Debug/net8.0/CTApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CTApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ef442684ed66eece411c59d45b33c92563510e6c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b46ef438d56087ae8e500923b0021cf5077e3451")]
 [assembly: System.Reflection.AssemblyProductAttribute("CTApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CTApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
