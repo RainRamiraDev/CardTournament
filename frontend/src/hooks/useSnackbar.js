32af8d2b669f6517f98bfdcfe1bf2ab5e4be1892
Rain Ramira <164517057+RainRamiraDev@users.noreply.github.com>
2025-06-12 16:19:26 -0300

    Update useSnackbar.js

--- frontend/src/hooks/useSnackbar.js
+++ frontend/src/hooks/useSnackbar.js
@@ -2,7 +2,7 @@ import { useState } from 'react';
 
 export const useSnackbar = () => {
   const [open, setOpen] = useState(false);
-  const [message, setMessage] = useState('');  // Estado para el mensaje personalizado
+  const [message, setMessage] = useState('');
 
   const showSnackbar = (msg) => {
     setMessage(msg);
